9061cac859a37848817a200ab95b83b68c644262
Randhi Putra <randhi@thoughtandfunction.com>
2025-07-15 11:38:46 +0700

    bump version to 2.0.1

--- app.config.js
+++ app.config.js
@@ -2,7 +2,7 @@ export default ({ config }) => ({
   ...config,
   name: 'moneytalk',
   slug: 'moneytalk',
-  version: '2.0.0',
+  version: '2.0.1',
   orientation: 'portrait',
   icon: './assets/images/icon.png',
   splash: {
